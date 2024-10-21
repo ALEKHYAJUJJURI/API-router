2e19b8f75933f9e67b7c69c693eb18d6d99df64f
Alekhya <jujjurialekhya123@gmail.com>
2024-10-21 15:46:41 +0530

    commit-4

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 
 import { HandleIndex } from './components/fakestore-index';
 import { Router } from 'express';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router >
+    <BrowserRouter >
     <HandleIndex/>
-    </Router>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
