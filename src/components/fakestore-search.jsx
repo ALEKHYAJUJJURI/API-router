8c3de6838f3960d737e6d179ad7804e1014d85ac
Alekhya <jujjurialekhya123@gmail.com>
2024-10-21 17:19:32 +0530

    commit-3

--- src/components/fakestore-search.jsx
+++ src/components/fakestore-search.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export function HandleSearch(){
     const [input,setInput] = useState('')
